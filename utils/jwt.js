8512b70074937250160215c7cbc6825dd3c820e9
wachira140 <wachiraw424@gmail.com>
2021-11-22 19:29:34 +0300

    added

--- utils/jwt.js
+++ utils/jwt.js
@@ -23,14 +23,14 @@ const createCookie = ({res, user, refreshToken})=>{
         expires: new Date(Date.now()+ oneDay),
         secure:process.env.NODE_ENV ==='production',
         signed:true,
-        sameSite: "none", secure
+        sameSite: "none",
     })
     res.cookie('refreshToken',refreshTokenJwt,{
         httpOnly:true,
         expires: new Date(Date.now()+ longerExp),
         secure:process.env.NODE_ENV ==='production',
         signed:true,
-        sameSite: "none", secure
+        sameSite: "none",
     })
 }
 
