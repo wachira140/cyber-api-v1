33436149358cb15f99efd7214eada4566aa8a503
wachira140 <wachiraw424@gmail.com>
2021-11-24 13:07:22 +0300

    comit

--- utils/jwt.js
+++ utils/jwt.js
@@ -23,6 +23,7 @@ const createCookie = ({res, user, refreshToken})=>{
         expires: new Date(Date.now()+ oneDay),
         secure:process.env.NODE_ENV ==='production',
         signed:true,
+        path:'https://jb-stores.netlify.app'
     })
     res.cookie('refreshToken',refreshTokenJwt,{
         httpOnly:true,
